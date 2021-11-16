98243f530809e6b388c2291ec4e348ff3bd3d965
ИСП-31 <-31@LAB24-05>
2021-11-16 14:32:42 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1;
+using Visual;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private WpfApp1.Triad _triada;
+        private WpfApp1.Triad _secondtriada;
+        private void Zapoln(object sender, RoutedEventArgs e)
+        {
+            
+            Random rnd = new Random();
+            _triada = new Triad() 
+            {
+                FirstNumber = rnd.Next(100),
+                SecondNumber = rnd.Next(100),
+                ThirdNumber = rnd.Next(100)
+            };
+            _secondtriada = new Triad()
+            {
+                FirstNumber = rnd.Next(100),
+                SecondNumber = rnd.Next(100),
+                ThirdNumber = rnd.Next(100)
+
+            };
+            data.ItemsSource= VisualArray.ToDataTable(_triada.TriadArray).DefaultView;
+            data_2.ItemsSource = VisualArray.ToDataTable(_secondtriada.TriadArray).DefaultView;
+        }
+        private void proverka(out int k)
+        {
+            int.TryParse(Chislo.Text, out k);
+            if (k == 0|| Chislo.Text==null) { MessageBox.Show("Введите ЧИСЛО!(не ноль)", "Ошибка"); return; }            
+        }
+        private void Proiz_Click(object sender, RoutedEventArgs e)
+        {
+            int inNum = Convert.ToInt32(Chislo.Text);
+            proverka(out int k);
+            WpfApp1.Triad thirdTriad = new Triad();
+            _triada.ProizTriad(inNum);
+            data_3.ItemsSource = VisualArray.ToDataTable(_triada.TriadArray).DefaultView;
+        }
+
+        private void Sum_Click(object sender, RoutedEventArgs e)
+        {
+            proverka(out int k);
+            int inNum = Convert.ToInt32(Chislo.Text);
+            _triada+=inNum;
+            data_3.ItemsSource = VisualArray.ToDataTable(_triada.TriadArray).DefaultView;
+        }
+
+        private void SumTriad_Click(object sender, RoutedEventArgs e)
+        {
+           Triad thirdtriada = _triada.SumTriad(_secondtriada);
+            data_3.ItemsSource = VisualArray.ToDataTable(thirdtriada.TriadArray).DefaultView;
+
+        }
+
+        private void Srav_Cklick(object sender, RoutedEventArgs e)
+        {
+            int inNum = Convert.ToInt32(Chislo.Text);
+            proverka(out int k);
+            string[] th=_triada.Compare(inNum);
+            data_3.ItemsSource = VisualArray.ToDataTable(th).DefaultView;
+        }
+
+
+    }
+}
---
+++ WpfApp1/Triada.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Triad
+    {
+        private int[] _triada;
+        public Triad()
+        {
+            _triada = new int[3];
+            _triada[0] = 0;
+            _triada[1] = 0;
+            _triada[2] = 0;
+        }
+        public Triad(int firstnum, int secondnum, int thirdnum)
+        {
+            _triada[0] = firstnum;
+            _triada[1] = secondnum;
+            _triada[2] = thirdnum;
+        }
+        public int FirstNumber { get => _triada[0]; set => _triada[0] = value; }
+        public int SecondNumber { get => _triada[1]; set => _triada[1] = value; }
+        public int ThirdNumber { get => _triada[2]; set => _triada[2] = value; }
+        public int[] TriadArray
+        {
+            get => _triada;
+            set
+            {
+                if (value.Length != 3) throw new Exception("The length of the entered array is not equal to 3", new ArrayTypeMismatchException());
+                _triada = value;
+            }
+        }
+        public void SumTriad(int inNum)
+        {
+            
+            for (int i = 0; i < 3; i++)
+            {
+                 _triada[i] += inNum;
+            }
+        }
+        public void ProizTriad(int inNum)
+        {
+           
+            for (int i = 0; i < 3; i++)
+            {
+                _triada[i] *= inNum;
+            }
+        }
+
+        public Triad SumTriad(Triad _secondtriada)
+        {
+            return new Triad()
+            {
+                FirstNumber = this.FirstNumber + _secondtriada.FirstNumber,
+                SecondNumber = this.SecondNumber + _secondtriada.SecondNumber,
+                ThirdNumber = this.ThirdNumber + _secondtriada.ThirdNumber
+            };
+        }
+        public string[] Compare(int number)
+        {
+            string[] th = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                if (_triada[i] != number) th[i] = "false";
+                else th[i] = "true";
+            }
+            return th;
+        }
+
+
+        public static Triad operator + (Triad _triada,int inNum)
+        {
+            _triada.FirstNumber += inNum;
+            _triada.SecondNumber += inNum;
+            _triada.ThirdNumber += inNum;
+            return _triada;
+        }
+
+
+
+    }
+}
